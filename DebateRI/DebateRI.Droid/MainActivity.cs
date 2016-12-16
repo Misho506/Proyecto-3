65bf1b4447921f7d86b87834886751123d85f7f0
Mauricio <maurisho01@gmail.com>
2016-12-16 01:25:39 -0600

    add icon

--- DebateRI/DebateRI.Droid/MainActivity.cs
+++ DebateRI/DebateRI.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace DebateRI.Droid
 {
-    [Activity(Label = "DebateRI", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "DebateRI", Icon = "@drawable/debate", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
